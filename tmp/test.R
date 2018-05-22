7075dc383a6f37270d98d54db4050e137f4c44d9
JarvisSu <jarvis_su@126.com>
2018-05-22 20:12:40 +0800

    more practice

--- tmp/test.R
+++ tmp/test.R
@@ -1,3 +1,17 @@
 myString <- "Hello, World!"
 print (myString)
 
+v<-2L
+print(class(v))
+
+v <- "TRUE"
+print(class(v))
+
+v <- charToRaw("Hello")
+print(class(v))
+
+# Create a vector.
+apple <- c('red','green',"yellow")
+print(apple)
+# Get the class of the vector.
+print(class(apple))
